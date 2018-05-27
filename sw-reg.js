1945909914b8a505be8964623c5cc683159a7630
Anas <anas2006101@gmail.com>
2018-05-27 03:08:03 +0300

    added it to the html directly

--- sw-reg.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DocumentContentLoaded" , event =>{
-    if(navigator.serviceWorker){
-        navigator.serviceWorker.register('sw.js')
-        .then(regsiteration => console.log("Service Registered",regsiteration))
-        .catch(err => console.log("Error: ",err));
-
-    }
-})
