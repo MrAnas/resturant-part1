b5db94efdb88b15edb86a4f6c42921566f1231bb
Anas <anas2006101@gmail.com>
2018-05-27 20:27:37 +0300

    updatin sw

--- sw.js
+++ sw.js
@@ -4,12 +4,12 @@ const urlToCache = [
     "./",
     "./sw-reg.js",
     "index.html",
-    "resturant.html",
+    "restaurant.html",
     "css/styles.css",
-    "data/resturants.json",
+    "data/restaurant.json",
     "js/dbhelper.js",
     "js/main.js",
-    "js/resturant_info.js",
+    "js/restaurant_info.js",
     "img/1.jpg",
     "img/2.jpg",
     "img/3.jpg",
@@ -46,7 +46,7 @@ self.addEventListener('activate', function(event) {
     );
   });
 
-self.addEventListener('fetch', function(event) {
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request)
         .then(function(response) {
@@ -54,8 +54,32 @@ self.addEventListener('fetch', function(event) {
           if (response) {
             return response;
           }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+  
+          // IMPORTANT: Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
